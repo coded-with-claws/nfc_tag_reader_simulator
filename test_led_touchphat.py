a90203079631cbc57156c89936b5f1f980cc1795
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2023-02-26 10:58:18 +0100

    test led touch phat

---
+++ test_led_touchphat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import signal
+import time
+
+import touchphat
+
+def access_granted():
+    led_enter_on_off()
+
+def access_denied():
+    led_back_blink()
+
+def led_enter_on_off():
+    touchphat.set_led('Enter', True)
+    time.sleep(1)
+    touchphat.set_led('Enter', False)
+
+def led_back_blink():
+    touchphat.set_led('Back', True)
+    time.sleep(0.1)
+    touchphat.set_led('Back', False)
+    time.sleep(0.1)
+    touchphat.set_led('Back', True)
+    time.sleep(0.1)
+    touchphat.set_led('Back', False)
+    time.sleep(0.1)
+    touchphat.set_led('Back', True)
+    time.sleep(0.1)
+    touchphat.set_led('Back', False)
+
+
+access_granted()
+time.sleep(1)
+access_denied()
+
